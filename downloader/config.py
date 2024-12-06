ab0f3a73e0d3922f637fdcace8848e4fa18cbf32
ker <116048644+yeartwothousandfive@users.noreply.github.com>
2024-12-06 11:16:08 +0800

    Update config.py

--- downloader/config.py
+++ downloader/config.py
@@ -1,3 +1,6 @@
+import os
+
+DOWNLOADS_FOLDER = os.path.join(os.path.expanduser("~"), "Downloads")
 
 YDL_OPTIONS = {
     'format': 'bestaudio/best',
@@ -6,6 +9,6 @@ YDL_OPTIONS = {
         'preferredcodec': 'mp3',
         'preferredquality': '192',
     }],
-    'outtmpl': '%(title)s.%(ext)s',  # Saves file with video title as filename
+    'outtmpl': os.path.join(DOWNLOADS_FOLDER, '%(title)s.%(ext)s'),  # Saves file with video title as filename
     'quiet': False,                  # Set to True to suppress logs
 }
