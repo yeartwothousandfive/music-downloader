9df3ba61ccd2c2c57ff0d4ff77a79a82f51cebb9
ker <116048644+yeartwothousandfive@users.noreply.github.com>
2024-12-06 13:17:29 +0800

    Update spotify_utils.py

--- downloader/spotify_utils.py
+++ downloader/spotify_utils.py
@@ -6,11 +6,12 @@ sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id="e7982a5028
                                                            client_secret="1114f775e3634c40bf5e40a886d7cb89"))
 
 def get_playlist_id(playlist_url):
-    match = re.search(r"playlist/([a-zA-Z0-9_-]+)", playlist_url)
+    match = re.search(r"playlist/([a-zA-Z0-9_-]+)", playlist_url.split('?')[0]) 
     if match:
         return match.group(1)
     else:
         raise ValueError("Invalid playlist URL")
+
 
 def get_songs_from_playlist(playlist_url):
     playlist_id = get_playlist_id(playlist_url)
